025a960711118d28d211f2a17665a74c63b28e6c
Black Jack <kavalchukmaksum@gmail.com>
2025-11-16 17:44:46 +0100

    logowanie i rejestracja w api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -80,3 +80,21 @@ class OfertaCreateSerializer(serializers.ModelSerializer):
             'data_wyslania'
         ]
     pass
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Uzytkownik
+        fields = ['username', 'email', 'password', 'password2', 'rola', 'nr_telefonu', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = Uzytkownik.objects.create_user(**validated_data)
+        return user
